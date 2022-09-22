760ea3b3ac0169866358964c8bbd4ec0a4ebfa9b
Xutai Ma <xutaima@gmail.com>
2022-09-22 12:06:25 -0700

    Create new agent types

--- simuleval/agents/__init__.py
+++ simuleval/agents/__init__.py
@@ -4,12 +4,8 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from . agent import Agent
-from . text_agent import TextAgent
-from . speech_agent import SpeechAgent
+from .agent import Agent
+from .text_agent import TextAgent
+from .speech_agent import SpeechToTextAgent, SpeechToSpeechAgent
 
-BUILDIN_AGENTS = [
-    Agent,
-    TextAgent,
-    SpeechAgent
-]
+BUILDIN_AGENTS = [Agent, TextAgent, SpeechToTextAgent, SpeechToSpeechAgent]
--- simuleval/agents/speech_agent.py
+++ simuleval/agents/speech_agent.py
@@ -4,9 +4,18 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-from . agent import Agent
+from .agent import Agent
 
 
-class SpeechAgent(Agent):
-    data_type = "speech"
+class SpeechToTextAgent(Agent):
+    source_type = "speech"
+    source_type = "text"
+
+    def __init__(self, args) -> None:
+        super().__init__(args)
+        self.source_segment_size = 10  # in ms
+
+
+class SpeechToSpeechAgent(Agent):
+    source_type = "speech"
     speech_segment_size = 10
