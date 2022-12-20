cf08f797351aba2a106083797912cfd3e87fb022
Xutai Ma <xutaima@gmail.com>
2022-12-19 18:40:33 -0800

    Remove empty segment check in pipeline

--- simuleval/agents/pipeline.py
+++ simuleval/agents/pipeline.py
@@ -42,11 +42,7 @@ class AgentPipeline(GenericAgent):
 
     def push(self, segment: Segment) -> None:
         for module in self.module_list[:-1]:
-            if not segment.is_empty:
-                module.push(segment)
-                segment = module.pop()
-            else:
-                return
+            segment = module.pushpop(segment)
         self.module_list[-1].push(segment)
 
     def pop(self) -> Segment:
