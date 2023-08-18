c06fb0c5b3aad2444c121e67119eac2fe54d1521
Mubaraq Sani <{ID}+{username}@users.noreply.github.com>
2023-08-18 22:18:04 +0100

    Add tgt language argument

--- examples/speech_to_text/eval.sh
+++ examples/speech_to_text/eval.sh
@@ -2,9 +2,6 @@ simuleval \
     --agent counter_in_tgt_lang_agent.py \
     --source-segment-size 1000 \
     --source source.txt --target reference/en.txt \
-    --tgt-lang tgt-lang.txt \
+    --tgt-lang reference/tgt_lang.txt \
     --output output 
-
-
-# default="en", type=str, choices=["en", "es", "de"]
-    # --tgt_lang tgt-lang.txt \
+    
