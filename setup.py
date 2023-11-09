ba792cca01e84d043714e317f0832b94dee8fc4b
Justin Haaheim <justinhaaheim@users.noreply.github.com>
2023-11-09 15:40:16 -0800

    Update setup.py sacrebleu dependency to be compatible with fairseq's (#85)
    When installing fairseq & simuleval in the same project they currently complain of conflicting dependencies, since fairseq depends on sacrebleu@master and sacrebleu just updated master from 2.3.1 to 2.3.2.
    
    This should allow them to both depend on 2.3.2.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "pytest",
         "pytest-cov",
-        "sacrebleu==2.3.1",
+        "sacrebleu>=2.3.1",
         "tornado",
         "soundfile",
         "pandas",
