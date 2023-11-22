9d42a66e49a05eb75b3dbf9d3e8589dd6617738b
Mubaraq Sani <{ID}+{username}@users.noreply.github.com>
2023-11-22 18:30:35 +0100

    Lint with Black

--- examples/speech_to_text/visualize.py
+++ examples/speech_to_text/visualize.py
@@ -3,21 +3,23 @@ import pandas as pd
 import argparse
 import pprint
 
+
 def read_scores_from_folder(folder_path):
-    score_file_path = os.path.join(folder_path, 'scores.tsv')
+    score_file_path = os.path.join(folder_path, "scores.tsv")
     if os.path.isfile(score_file_path):
         with open(score_file_path, "r") as f:
             contents = [line.strip() for line in f.read().split("\n") if line.strip()]
         return contents
     else:
         return None
-    
+
+
 def read_scores_files(output_folder):
     all_contents = []
 
     if not os.path.isdir(output_folder):
         raise ValueError("Output folder does not exist")
-    
+
     output_folder = os.path.abspath(output_folder)
 
     for folder in os.listdir(output_folder):
@@ -29,12 +31,13 @@ def read_scores_files(output_folder):
                 all_contents.append(contents)
     return all_contents
 
+
 def process_result(output_folder, metric_names):
     all_contents = read_scores_files(output_folder)
-    
+
     # Extracting headers from the first line of each "scores.tsv" file
     headers = [contents[0].split() for contents in all_contents if contents]
-    
+
     if not headers:
         raise ValueError("No headers found in the results")
     reference_header = headers[0]
@@ -45,7 +48,7 @@ def process_result(output_folder, metric_names):
 
     if not common_metrics:
         raise ValueError("No common metrics found in the results")
-    
+
     # Extracting scores for each metric
     scores = []
     for contents in all_contents:
@@ -64,10 +67,13 @@ def process_result(output_folder, metric_names):
 
     return filtered_df
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--output", type=str, default=None, help="Output directory")
-    parser.add_argument("--metrics", type=str, nargs="+", default=None, help="Metrics to be extracted")
+    parser.add_argument(
+        "--metrics", type=str, nargs="+", default=None, help="Metrics to be extracted"
+    )
     args = parser.parse_args()
 
     df = process_result(args.output, args.metrics)
